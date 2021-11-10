78646b2fa87f319385c9ca547fdfc663a5d6b47a
Ramyak Mehra <rmehra_be19@thapar.edu>
2021-11-10 16:48:40 +0530

    exposed memory storage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.1
+- Fixed typos in readme
+- Exposed memory storage in the lib
 ## 1.0.0
 
 - Initial version.
--- lib/shelf_rate_limiter.dart
+++ lib/shelf_rate_limiter.dart
@@ -2,3 +2,4 @@ library shelf_rate_limiter;
 
 export 'src/shelf_rate_limiter_base.dart';
 export 'src/storage/base_storage.dart';
+export 'src/storage/memory_storage.dart';
